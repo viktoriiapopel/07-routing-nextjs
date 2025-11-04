dacba1576960320d110bc496857257b6c339fc63
Viktoriia Popel <xructa2009@gmail.com>
2025-11-04 11:02:56 +0100

    fix

--- app/notes/page.tsx
+++
@@ -1,9 +0,0 @@
-import NoteList from "@/components/NoteList/NoteList";
-import { fetchNotes } from "../../lib/api";
-
-const Notes = async () => {
-  const res = await fetchNotes();
-
-  return <NoteList notes={res.notes} />;
-};
-export default Notes;
